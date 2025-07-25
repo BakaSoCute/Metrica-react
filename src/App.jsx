b45f3c86d379b68cf25b21664097b17fec1e5e7c
Aleksandr Omelucik <bladewizard@mail.ru>
2025-07-25 20:09:12 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Heading from "./Header"
+
+function App() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+function Heading() {
+  return (
+    <h1>Современные и стильные интерфейсы для комфортной жизни</h1>
+  )
+}
+
+export default Heading;
